e01fe8e99f79a3a8140ea3b61bfbad9b13fe5273
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-06 12:06:41 -0400

    Add sell feature, finish cleaning up

--- game.py
+++ game.py
@@ -6,22 +6,26 @@ teeth = {
 scissors = {
 	'name': 'scissors',
 	'cost': 5,
-	'earn': 5
+	'earn': 5,
+	'used': 2
 }
 push = {
 	'name': 'push mower',
 	'cost': 25,
-	'earn': 50
+	'earn': 50,
+	'used': 10
 }
 battery = {
 	'name': 'battery powered lawnmower',
 	'cost': 250,
-	'earn': 100
+	'earn': 100,
+	'used': 100
 }
 students = {
 	'name': 'students',
 	'cost': 500,
-	'earn': 250
+	'earn': 250,
+	'used': 200
 }
 
 current_tools = [teeth]
@@ -31,6 +35,7 @@ day = 0
 money = 0
 lifetime_earnings = 0
 tool = current_tools[0]
+exited = False
 
 
 def print_current_tool(t):
@@ -55,7 +60,9 @@ while not (money >= 1000 and students in current_tools):
 "goal" to see your objective for winning
 "buy TOOL" to buy chosen tool
 "use TOOL" to equip chosen tool
+"sell TOOL" to sell chosen tool for used price
 "restart" to restart game
+"exit" to quit
 ---------------''')
 	
 	elif selection == 'tool':
@@ -81,7 +88,7 @@ while not (money >= 1000 and students in current_tools):
 		money += tool['earn']
 		lifetime_earnings += tool['earn']
 		day += 1
-		print(f'''You earned ${tool['earn']}\nPocket: {money}''')
+		print(f'''You earned ${tool['earn']}\nPocket: ${money}''')
 		print('---------------')
 	elif selection == 'goal':
 		print('---------------')
@@ -158,12 +165,57 @@ while not (money >= 1000 and students in current_tools):
 				elif tool_to_use in current_tools:
 					tool = tool_to_use
 					print('---------------')
-					print(f'''now using {tool['name']}, "mow" to earn {tool['earn']}''')
+					print(f'''now using {tool['name']}, "mow" to earn ${tool['earn']}''')
 					print('---------------')
 				else:
 					print('---------------')
 					print(f'''you do not own {tool_to_use['name']}. "buy {tool_to_use['name']}" to purchase''')
 					print('---------------')
+	
+	elif selection.startswith('sell'):
+		if 'tool' in selection:
+			print('---------------')
+			print('not literally "TOOL"!\n"available tools" will help')
+			print('---------------')
+		else:
+			tool_to_sell = None
+			if 'teeth' in selection:
+				tool_to_sell = teeth
+			elif 'scis' in selection:
+				tool_to_sell = scissors
+			elif 'push' in selection:
+				tool_to_sell = push
+			elif 'batt' in selection:
+				tool_to_sell = battery
+			elif 'stud' in selection:
+				tool_to_sell = students
+			
+			if not tool_to_sell:
+				print('---------------')
+				print('invalid selection, "current tools" will help')
+				print('---------------')
+			else:
+				if tool_to_sell == teeth:
+					print('---------------')
+					print('you will need those to eat')
+					print('---------------')
+					
+				elif tool_to_sell in current_tools:
+					money += tool_to_sell['cost']
+					lifetime_earnings += tool_to_sell['cost']
+					current_tools.remove(tool_to_sell)
+					available_tools.append(tool_to_sell)
+					if tool_to_sell == tool:
+						tool = teeth
+					print('---------------')
+					print(f'''Sold {tool_to_sell['name']}! "buy {tool_to_sell['name']}" to re-buy''')
+					print(f'''+${tool_to_sell['cost']}\nCurrent money: ${money}''')
+					print('---------------')
+				else:
+					print('---------------')
+					print(f'''you don't own {tool_to_sell['name']}. "buy {tool_to_sell['name']}" to buy''')
+					print('---------------')
+					
 	elif selection == 'restart':
 		current_tools = [teeth]
 		available_tools = [scissors, push, battery, students]
@@ -175,12 +227,20 @@ while not (money >= 1000 and students in current_tools):
 		print('---------------')
 		print('restarting...')
 		print('---------------')
-		
+	
+	elif selection == 'exit' or selection == 'quit':
+		exited = True
+		break
 	else:
 		print('---------------')
 		print('invalid selection\n"menu" for options')
 		print('---------------')
 
-print('---------------')
-print(f'''you won in {day} days''')
-print('---------------')
+if exited:
+	print('---------------')
+	print('exiting...')
+	print('---------------')
+else:
+	print('\n---------------')
+	print(f'''you won in {day} days''')
+	print('---------------')
